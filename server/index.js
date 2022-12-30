80545649588ee3d6dc4f85ba52d2268727948861
Noah Lett <noahblett96@gmail.com>
2022-12-30 23:02:06 +0000

    Added submit method to Registration component and drafted post method on server side.

--- server/index.js
+++ server/index.js
@@ -1,26 +1,45 @@
 require('dotenv/config');
-// const path = require('path');
-// const pg = require('pg');
-// const argon2 = require('argon2');
+const pg = require('pg');
+const argon2 = require('argon2');
 const express = require('express');
-// const ClientError = require('./client-error');
+const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
 
-// const db = new pg.Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
-// const publicPath = path.join(__dirname, 'public');
-
 app.use(staticMiddleware);
+app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.post('/api/auth/sign-up', (req, res, next) => {
+  const { firstName, lastName, city, state, username, password, confirmPassword } = req.body;
+  if (!firstName || !lastName || !city || !state || !username || !password || !confirmPassword) {
+    throw new ClientError(400, 'All fields are required.');
+  } else if (password !== confirmPassword) {
+    throw new ClientError(400, 'Please ensure your password matches with the confirmation field.');
+  }
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+        insert into "users" ("firstName", "lastName", "username", "hashedPassword", "city", "state")
+        values ($1, $2, $3, $4, $5, $6)
+        returning "userId", "username", "createdAt"
+        `;
+      const params = [firstName, lastName, username, hashedPassword, city, state];
+      return db.query(sql, params);
+    })
+    .then(result => {
+      const [user] = result.rows;
+      res.status(201).json(user);
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
