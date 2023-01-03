e41a38ee6fc10fe24c626412fbcfc56ef95cbbd3
Noah Lett <noahblett96@gmail.com>
2023-01-03 17:11:44 +0000

    Added basic home page and implemented carousel component

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -11,7 +11,7 @@ export default class Carousel extends React.Component {
     this.handleRight = this.handleRight.bind(this);
     this.viewJump = this.viewJump.bind(this);
     this.infiniteScroll = this.infiniteScroll.bind(this);
-    this.automatedSwap = this.automatedSwap.bind(this);
+    // this.automatedSwap = this.automatedSwap.bind(this);
   }
 
   handleLeft() {
@@ -23,7 +23,7 @@ export default class Carousel extends React.Component {
       }
     }
     clearInterval(this.swapper);
-    this.automatedSwap();
+    // this.automatedSwap();
   }
 
   handleRight() {
@@ -35,13 +35,13 @@ export default class Carousel extends React.Component {
       }
     }
     clearInterval(this.swapper);
-    this.automatedSwap();
+    // this.automatedSwap();
   }
 
   viewJump(event) {
     this.setState({ viewIndex: Number(event.target.id) });
     clearInterval(this.swapper);
-    this.automatedSwap();
+    // this.automatedSwap();
   }
 
   infiniteScroll() {
@@ -52,29 +52,29 @@ export default class Carousel extends React.Component {
     }
   }
 
-  automatedSwap() {
-    this.swapper = setInterval(this.infiniteScroll, 3000);
-  }
+  // automatedSwap() {
+  //   this.swapper = setInterval(this.infiniteScroll, 3000);
+  // }
 
   render() {
     return (
-      <div className='carousel-box w-75 pt-4 pb-2'>
+      <div className='carousel-box w-75 pt-5 pb-3 w-100'>
         <div className='row d-flex flex-wrap justify-content-center'>
           <div className='col-md-3 d-flex justify-content-center align-items-center'>
-            <i className="fa-solid fa-chevron-left" onClick={this.handleLeft} />
+            <i className="fa-solid fa-chevron-left fs-1" onClick={this.handleLeft} />
           </div>
-          <div className='col-md-6'>
+          <div className='col-md-6 d-flex flex-column justify-content-center align-items-center'>
             <img className='carousel-image' src={dataSets[this.state.viewIndex].imageUrl} alt="Carousel Image" />
-            <h6>{dataSets[this.state.viewIndex].text}</h6>
+            <h6 className='py-3'>{dataSets[this.state.viewIndex].text}</h6>
           </div>
           <div className='col-md-3 d-flex justify-content-center align-items-center'>
-            <i className="fa-solid fa-chevron-right" onClick={this.handleRight} />
+            <i className="fa-solid fa-chevron-right fs-1" onClick={this.handleRight} />
           </div>
           <div className='d-flex flex-wrap justify-content-center'>
             <span className='circles'>
-              <i key={0} className={this.state.viewIndex === 0 ? 'fa-solid fa-circle' : 'fa-regular fa-circle'} onClick={this.viewJump}/>
-              <i key={1} className={this.state.viewIndex === 1 ? 'fa-solid fa-circle' : 'fa-regular fa-circle'} onClick={this.viewJump} />
-              <i key={2} className={this.state.viewIndex === 2 ? 'fa-solid fa-circle' : 'fa-regular fa-circle'} onClick={this.viewJump} />
+              <i id='0' key={0} className={this.state.viewIndex === 0 ? 'fa-solid fa-circle px-1 fs-5' : 'fa-regular fa-circle px-1 fs-5'} onClick={this.viewJump}/>
+              <i id='1' key={1} className={this.state.viewIndex === 1 ? 'fa-solid fa-circle px-1 fs-5' : 'fa-regular fa-circle px-1 fs-5'} onClick={this.viewJump} />
+              <i id='2' key={2} className={this.state.viewIndex === 2 ? 'fa-solid fa-circle px-1 fs-5' : 'fa-regular fa-circle px-1 fs-5'} onClick={this.viewJump} />
             </span>
           </div>
         </div>
