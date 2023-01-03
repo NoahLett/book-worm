124bab1f3535c63900092fdaeea529aa0de828ab
Noah Lett <noahblett96@gmail.com>
2023-01-03 17:43:12 +0000

    Added sign-in page and began work on authentication React component

---
+++ client/pages/sign-in.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class Authentication extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='d-flex justify-content-center'>
+          <h1 className='text-center my-5 w-50'><strong>Welcome Back!</strong></h1>
+        </div>
+        <h3 className='text-center'><strong>Please sign in to gain access to user features.</strong></h3>
+        <div className='form-container d-flex justify-content-center mt-4 py-3'>
+          <form className='sign-in-form' onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="username" className='form-label'>Username</label>
+              <input
+                required
+                autoFocus
+                id='username'
+                name='username'
+                type="text"
+                onChange={this.handleChange}
+                className="form-control" />
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+}
