9650edda4a654989bcdda8e3663bf8153b1f9193
Noah Lett <noahblett96@gmail.com>
2023-01-04 22:39:32 +0000

    Implemented all inputs for post-form and improved layout

--- client/pages/post-form.jsx
+++ client/pages/post-form.jsx
@@ -4,15 +4,14 @@ export default class PostForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: '',
+      image: 'https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg',
       title: '',
       isbn: '',
       comments: '',
-      postType: 'sale',
-      trading: 'false'
+      postType: 'sale'
     };
-    // this.fileInputRef = React.createRef();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fileInputRef = React.createRef();
+    // this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -24,40 +23,83 @@ export default class PostForm extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <form className='post-form' onSubmit={this.handleSubmit}>
-            <div className='col-3'>
+        <h1 className='text-center my-5'>Create or Edit your Post!</h1>
+        <div className='d-flex justify-content-center align-items-center my-5'>
+          <div className='col-3 mx-5'>
+            <img className='w-100 h-100' src={this.state.image} alt="post-image" />
+          </div>
+          <div className='col-5 bg-light my-5'>
+            <form className='post-form' onSubmit={this.handleSubmit}>
               <div>
-                <img src={this.state.image} alt="post-image" />
-              </div>
-            </div>
-            <div className='col-6'>
-              <div>
-                <label htmlFor="title" className='form-label'>Title</label>
-                <input
+                <div>
+                  <label htmlFor="title" className='form-label'>Title</label>
+                  <input
                 required
                 autoFocus
                 id='title'
                 name='title'
                 type="text"
                 onChange={this.handleChange}
-                className="form-control" />
-              </div>
-              <div>
-                <label htmlFor="isbn" className='form-label'>ISBN</label>
-                <input
+                className="form-control w-50" />
+                </div>
+                <div>
+                  <label htmlFor="isbn" className='form-label'>ISBN</label>
+                  <input
                   required
                   type="text"
                   name="isbn"
                   id="isbn"
                   onChange={this.handleChange}
-                  className="form-control" />
+                  className="form-control w-50" />
+                </div>
+                <div>
+                  <label htmlFor="comments" className='form-label'>Comments</label>
+                  <textarea
+                  required
+                  name="comments"
+                  id="comments"
+                  maxLength="225"
+                  onChange={this.handleChange}
+                  className="form-control w-75" />
+                </div>
+                <div className='input-group d-flex justify-content-around w-50'>
+                  <div>
+                    <label htmlFor="sale" className='form-label'>For Sale</label>
+                    <input
+                  defaultChecked
+                  value='sale'
+                  name='postType'
+                  id='postType'
+                  onChange={this.handleChange}
+                  type="radio"
+                  className='form-check-input' />
+                  </div>
+                  <div>
+                    <label htmlFor="want" className='form-label'>Wanted</label>
+                    <input
+                    value='want'
+                    name="postType"
+                    id="postType"
+                    onChange={this.handleChange}
+                    type="radio"
+                    className='form-check-input' />
+                  </div>
+                </div>
+                <div>
+                  <label htmlFor="image-upload" className='form-label'>Image Upload</label>
+                  <input
+                  required
+                  type="file"
+                  name='image'
+                  ref={this.fileInputRef}
+                  accept=".png, .jpg, .jpeg" />
+                </div>
+                <div className='d-flex justify-content-end m-3'>
+                  <button type='submit' className='btn btn-outline-info'>Post</button>
+                </div>
               </div>
-              <div>
-                <label htmlFor="comments" className='form-label'>Comments</label>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     );
