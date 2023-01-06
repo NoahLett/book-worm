ebb515a195ee3630dc488870f293578864660a25
Noah Lett <noahblett96@gmail.com>
2023-01-06 19:50:33 +0000

    Converted Want and Sale functions to class components and implemented conditional ellipsis render based on the current signed-in user

--- client/pages/wanted.jsx
+++ client/pages/wanted.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppContext from '../lib/app-context';
 
 const styles = {
   image: {
@@ -8,6 +9,9 @@ const styles = {
   textBox: {
     backgroundColor: '#0096c7',
     borderRadius: '10px'
+  },
+  dots: {
+    cursor: 'pointer'
   }
 };
 
@@ -46,19 +50,57 @@ export default class Wanted extends React.Component {
   }
 }
 
-function Want(props) {
-  const { wantTitle, wantPhotoFile, wantContent, isbn, city, state } = props.want;
-  return (
-    <div className='card mb-4 shadow-sm'>
-      <img className='card-img-top py-3 bg-secondary' src={wantPhotoFile} alt={wantTitle} style={styles.image}/>
-      <div className='card-body'>
-        <h5 className='card-title'>{wantTitle}</h5>
-        <h6 className='card-text text-secondary'>{`ISBN: ${isbn}`}</h6>
-        <div style={styles.textBox}>
-          <p className='text-white p-3'>{wantContent}</p>
+class Want extends React.Component {
+
+  render() {
+    const { user } = this.context;
+    const { userId, wantTitle, wantPhotoFile, wantContent, isbn, city, state } = this.props.want;
+    if (!user) {
+      return (
+        <div className='card mb-4 shadow-sm'>
+          <img className='card-img-top py-3 bg-secondary' src={wantPhotoFile} alt={wantTitle} style={styles.image} />
+          <div className='card-body'>
+            <h5 className='card-title'>{wantTitle}</h5>
+            <h6 className='card-text text-secondary'>{`ISBN: ${isbn}`}</h6>
+            <div style={styles.textBox}>
+              <p className='text-white p-3'>{wantContent}</p>
+            </div>
+            <p className='card-text text-secondary text-end'>{`${city}, ${state}`}</p>
+          </div>
         </div>
-        <p className='card-text text-secondary text-end'>{`${city}, ${state}`}</p>
-      </div>
-    </div>
-  );
+      );
+    } else if (user.userId !== userId) {
+      return (
+        <div className='card mb-4 shadow-sm'>
+          <img className='card-img-top py-3 bg-secondary' src={wantPhotoFile} alt={wantTitle} style={styles.image} />
+          <div className='card-body'>
+            <h5 className='card-title'>{wantTitle}</h5>
+            <h6 className='card-text text-secondary'>{`ISBN: ${isbn}`}</h6>
+            <div style={styles.textBox}>
+              <p className='text-white p-3'>{wantContent}</p>
+            </div>
+            <p className='card-text text-secondary text-end'>{`${city}, ${state}`}</p>
+          </div>
+        </div>
+      );
+    } else if (user.userId === userId) {
+      return (
+        <div className='card mb-4 shadow-sm'>
+          <img className='card-img-top py-3 bg-secondary' src={wantPhotoFile} alt={wantTitle} style={styles.image} />
+          <div className='card-body'>
+            <div className='d-flex justify-content-end'>
+              <i className="fa-solid fa-ellipsis fs-3" style={styles.dots} />
+            </div>
+            <h5 className='card-title'>{wantTitle}</h5>
+            <h6 className='card-text text-secondary'>{`ISBN: ${isbn}`}</h6>
+            <div style={styles.textBox}>
+              <p className='text-white p-3'>{wantContent}</p>
+            </div>
+            <p className='card-text text-secondary text-end'>{`${city}, ${state}`}</p>
+          </div>
+        </div>
+      );
+    }
+  }
 }
+Want.contextType = AppContext;
