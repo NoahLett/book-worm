fe67d726a42246039d273ec1b1e749234f93e23c
Noah Lett <noahblett96@gmail.com>
2023-03-13 21:30:34 +0000

    Made improvements to responsive layout of home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -12,20 +12,23 @@ const styles = {
 
 export default function Home(props) {
   return (
-    <div>
+    <div className='main'>
+      <div className="overlay" />
       <video className='home-video' src="/videos/grass-video.mp4" autoPlay loop muted playsInline type='video/mp4' />
-      <h1 className='header'>Welcome to BookWorm!</h1>
-      <h3 className='lower-header'>No More Over-Priced Textbooks</h3>
-      <div className="parallelogram-container">
-        <div className="parallelogram">
-          <FaHandshake className='handshake'/>
-          <h4 className="sub-header">Have a textbook to sell?</h4>
-          <a href="#sign-up"><button className="button-dark" style={styles.button}>Get Started</button></a>
-        </div>
-        <div className="parallelogram">
-          <GiTakeMyMoney className='money'/>
-          <h4 className="sub-header">Trying to save this year?</h4>
-          <a href="#for-sale"><button className="button-dark" style={styles.button}>Search Deals</button></a>
+      <div className="content">
+        <h1 className='header'>Welcome to BookWorm!</h1>
+        <h3 className='lower-header'>No More Over-Priced Textbooks</h3>
+        <div className="parallelogram-container">
+          <div className="parallelogram">
+            <FaHandshake className='handshake'/>
+            <h4 className="sub-header">Have a textbook to sell?</h4>
+            <a href="#sign-up"><button className="button-dark" style={styles.button}>Get Started</button></a>
+          </div>
+          <div className="parallelogram">
+            <GiTakeMyMoney className='money'/>
+            <h4 className="sub-header">Trying to save this year?</h4>
+            <a href="#for-sale"><button className="button-dark" style={styles.button}>Search Deals</button></a>
+          </div>
         </div>
       </div>
     </div>
