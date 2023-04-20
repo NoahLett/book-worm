ad9c6f7de7a410251c6aabe45acf0e752ae2268b
Noah Lett <noahblett96@gmail.com>
2023-04-20 03:31:19 +0000

    Improved layout of city and state inputs

--- client/pages/sign-up.jsx
+++ client/pages/sign-up.jsx
@@ -91,7 +91,7 @@ export default function Registration() {
     <div className="signup-container">
       {success
         ? (
-          <div className="section">
+          <div className="signup-section">
             <h1 className="success">Success!</h1>
             <p>
               <a href="#sign-in" className='sign-in-link'>Sign In</a>
@@ -99,7 +99,7 @@ export default function Registration() {
           </div>
           )
         : (
-          <div className="section">
+          <div className="signup-section">
             <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>{errMsg}</p>
             <h1 className="signup-header">Register</h1>
             <form className="signup-form" onSubmit={handleSubmit}>
@@ -125,79 +125,83 @@ export default function Registration() {
                 onChange={e => setLastName(e.target.value)}
                 required
                 className="input-field"/>
-
-              <label htmlFor="city" className="signup-label">
-                City:
-              </label>
-              <input
+              <div className='input-box'>
+                <div className='city-box'>
+                  <label htmlFor="city" className="signup-label">
+                    City:
+                  </label>
+                  <input
                 type="text"
                 id='city'
                 autoComplete='off'
                 onChange={e => setCity(e.target.value)}
                 required
                 className="input-field"/>
+                </div>
 
-              <label htmlFor="state" className="signup-label">
-                State:
-              </label>
-              <select
+                <div className="state-boc">
+                  <label htmlFor="state" className="signup-label">
+                    State:
+                  </label>
+                  <select
                 required
                 id="state"
                 onChange={e => setState(e.target.value)}
                 className='input-field'>
-                <option value="" defaultValue="">Choose...</option>
-                <option value="AL">AL</option>
-                <option value="AK">AK</option>
-                <option value="AZ">AZ</option>
-                <option value="AR">AR</option>
-                <option value="CA">CA</option>
-                <option value="CO">CO</option>
-                <option value="CT">CT</option>
-                <option value="DE">DE</option>
-                <option value="FL">FL</option>
-                <option value="GA">GA</option>
-                <option value="HI">HI</option>
-                <option value="ID">ID</option>
-                <option value="IL">IL</option>
-                <option value="IN">IN</option>
-                <option value="IA">IA</option>
-                <option value="KS">KS</option>
-                <option value="KY">KY</option>
-                <option value="LA">LA</option>
-                <option value="ME">ME</option>
-                <option value="MD">MD</option>
-                <option value="MA">MA</option>
-                <option value="MI">MI</option>
-                <option value="MN">MN</option>
-                <option value="MS">MS</option>
-                <option value="MO">MO</option>
-                <option value="MT">MT</option>
-                <option value="NE">NE</option>
-                <option value="NV">NV</option>
-                <option value="NH">NH</option>
-                <option value="NJ">NJ</option>
-                <option value="NM">NM</option>
-                <option value="NY">NY</option>
-                <option value="NC">NC</option>
-                <option value="ND">ND</option>
-                <option value="OH">OH</option>
-                <option value="OK">OK</option>
-                <option value="OR">OR</option>
-                <option value="PA">PA</option>
-                <option value="RI">RI</option>
-                <option value="SC">SC</option>
-                <option value="SD">SD</option>
-                <option value="TN">TN</option>
-                <option value="TX">TX</option>
-                <option value="UT">UT</option>
-                <option value="VT">VT</option>
-                <option value="VA">VA</option>
-                <option value="WA">WA</option>
-                <option value="WV">WV</option>
-                <option value="WI">WI</option>
-                <option value="WY">WY</option>
-              </select>
-
+                    <option value="" defaultValue="">Choose...</option>
+                    <option value="AL">AL</option>
+                    <option value="AK">AK</option>
+                    <option value="AZ">AZ</option>
+                    <option value="AR">AR</option>
+                    <option value="CA">CA</option>
+                    <option value="CO">CO</option>
+                    <option value="CT">CT</option>
+                    <option value="DE">DE</option>
+                    <option value="FL">FL</option>
+                    <option value="GA">GA</option>
+                    <option value="HI">HI</option>
+                    <option value="ID">ID</option>
+                    <option value="IL">IL</option>
+                    <option value="IN">IN</option>
+                    <option value="IA">IA</option>
+                    <option value="KS">KS</option>
+                    <option value="KY">KY</option>
+                    <option value="LA">LA</option>
+                    <option value="ME">ME</option>
+                    <option value="MD">MD</option>
+                    <option value="MA">MA</option>
+                    <option value="MI">MI</option>
+                    <option value="MN">MN</option>
+                    <option value="MS">MS</option>
+                    <option value="MO">MO</option>
+                    <option value="MT">MT</option>
+                    <option value="NE">NE</option>
+                    <option value="NV">NV</option>
+                    <option value="NH">NH</option>
+                    <option value="NJ">NJ</option>
+                    <option value="NM">NM</option>
+                    <option value="NY">NY</option>
+                    <option value="NC">NC</option>
+                    <option value="ND">ND</option>
+                    <option value="OH">OH</option>
+                    <option value="OK">OK</option>
+                    <option value="OR">OR</option>
+                    <option value="PA">PA</option>
+                    <option value="RI">RI</option>
+                    <option value="SC">SC</option>
+                    <option value="SD">SD</option>
+                    <option value="TN">TN</option>
+                    <option value="TX">TX</option>
+                    <option value="UT">UT</option>
+                    <option value="VT">VT</option>
+                    <option value="VA">VA</option>
+                    <option value="WA">WA</option>
+                    <option value="WV">WV</option>
+                    <option value="WI">WI</option>
+                    <option value="WY">WY</option>
+                  </select>
+                </div>
+              </div>
               <label className='signup-label' htmlFor="username">
                 Username:
                 <span className={validName ? 'valid' : 'hide'}>
