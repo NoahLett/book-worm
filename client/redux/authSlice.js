2abf2509018f1af4b0c0aa68af91de62def6c86c
Noah Lett <noahblett96@gmail.com>
2023-05-09 00:22:01 +0000

    Created authSlice object with various login and logout possibilities

---
+++ client/redux/authSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginRequest: state => {
+      state.loading = true;
+      state.error = null;
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+      state.error = null;
+    },
+    loginFailure: (state, action) => {
+      state.loading = false;
+      state.user = null;
+      state.error = action.payload;
+    },
+    logout: state => {
+      state.user = null;
+      state.error = null;
+    }
+  }
+});
